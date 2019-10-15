8e84344454872c79021d55d985c0fa420a91bff6
jeangjenq <jeangjenq@hotmail.com>
2019-10-15 23:22:17 +1300

    Version 2.2
        -Now can change directory multiple times while keeping file paths up to date.
    To do:
        Haven't figure out a way for it to not convert all the file paths to absolute so better select the tcl/python linked nodes and exclude them!

--- setProjDir.py
+++ setProjDir.py
@@ -80,9 +80,10 @@ def setProjDir(var):
     else:
         filepath = '[join [lrange [split [file dirname [knob root.name]] "/"] 0 end-4] "/"]'
 
-
+    absFilePaths(nodeWithFile())
     nuke.root().knob("project_directory").setValue(filepath)
     searchReplaceProjDir(selectNodesPanel())
+
 
 def absFilePathsSel():
     try:
